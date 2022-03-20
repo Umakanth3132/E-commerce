80650ed7f8116e6ede9d0685bcde1b0904b8fffb
Umakanth <umakanthreddy.m@gmail.com>
2022-03-20 10:39:07 -0400

    ConfigServer added
    Signed-off-by: Umakanth <umakanthreddy.m@gmail.com>

---
+++ CofigServer/src/test/java/com/dailycodebuffer/CofigServer/CofigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.CofigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
